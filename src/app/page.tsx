934e8af339f20bfc321462b309a9f9d60a8b6cd1
project774fan <chinari819y2k@gmail.com>
2024-04-23 23:59:31 +0900

    meta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,34 @@
 import Image from "next/image";
 import "./globals.css";
-import Link from "next/link";
+
 import imglist from "@/components/imglist";
+import Head from "next/head";
 
 export default function Home() {
   const img = imglist();
 
   return (
-    <div className="grid grid-cols-8">
-      {img.map((imgSrc, index) => (
-        <div key={index}>
-          <a href="https://youtu.be/e6DFAePfAIc?si=ClFpB4SCP_nhFAqB" target="_blank">
-            <img
-              src={imgSrc}
-              alt="popo"
-              className="h-full w-full transition-transform duration-300 hover:scale-105 hover:rounded-xl"
-            />
-          </a>
-        </div>
-      ))}
+    <div>
+      <Head>
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@chinari819" />
+        <meta name="twitter:title" content="ポポニウム" />
+        <meta name="twitter:description" content="ポポニウム" />
+        <meta name="twitter:image" content="img/ieiri.jpg" />
+      </Head>
+      <div className="grid grid-cols-8">
+        {img.map((imgSrc, index) => (
+          <div key={index}>
+            <a href="https://youtu.be/e6DFAePfAIc?si=ClFpB4SCP_nhFAqB" target="_blank">
+              <img
+                src={imgSrc}
+                alt="popo"
+                className="h-full w-full transition-transform duration-300 hover:scale-105 hover:rounded-xl"
+              />
+            </a>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
